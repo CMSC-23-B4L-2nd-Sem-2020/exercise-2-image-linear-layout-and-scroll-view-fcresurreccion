80df95d517b55d82a805a13e86768806040f1de5
fcresurreccion <fcresurreccion@up.edu.ph>
2020-02-13 13:14:03 +0800

    chore: add first commit

---
+++ app/src/main/java/com/example/exer2_resurreccion_b4l/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.exer2_resurreccion_b4l
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
