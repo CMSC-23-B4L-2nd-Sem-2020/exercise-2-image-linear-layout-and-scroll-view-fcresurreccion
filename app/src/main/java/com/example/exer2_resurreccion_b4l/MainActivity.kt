9fd4c41016ba7944c78ddf9b89af4ee88bc4e3ca
fcresurreccion <fcresurreccion@up.edu.ph>
2020-02-13 14:18:13 +0800

    feat: add image and bio randomizer

--- app/src/main/java/com/example/exer2_resurreccion_b4l/MainActivity.kt
+++ app/src/main/java/com/example/exer2_resurreccion_b4l/MainActivity.kt
@@ -2,11 +2,77 @@ package com.example.exer2_resurreccion_b4l
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    /*
+    * Declare variables for image and text views
+    * */
+    private lateinit var imageView : ImageView
+    private lateinit var textViewBio : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        * Get button view from layout and set click listener
+        * */
+        val randomButton: Button = findViewById(R.id.random_button)
+        randomButton.setOnClickListener { randomize() }
+
+        /*
+        * Get image and text view from layout
+        * */
+        imageView = findViewById(R.id.disney_image)
+        textViewBio = findViewById(R.id.disney_bio_text)
+    }
+
+    /*
+    * Click listener for the random button
+    * */
+    private fun randomize() {
+        val randomInt: Int = (1..6).random()
+
+        /*
+        * Get image of randomized character from drawable
+        * */
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.sora
+            2 -> R.drawable.riku
+            3 -> R.drawable.kairi
+            4 -> R.drawable.roxas
+            5 -> R.drawable.namine
+            6 -> R.drawable.xion
+            7 -> R.drawable.mickey
+            8 -> R.drawable.donald
+            9 -> R.drawable.goofy
+            else -> R.drawable.yensid
+        }
+
+        /*
+        * Get bio of randomized character from values/strings
+        * */
+        val stringResourceBio = when(randomInt) {
+            1 -> R.string.bio_sora
+            2 -> R.string.bio_riku
+            3 -> R.string.bio_kairi
+            4 -> R.string.bio_roxas
+            5 -> R.string.bio_namine
+            6 -> R.string.bio_xion
+            7 -> R.string.bio_mickey
+            8 -> R.string.bio_donald
+            9 -> R.string.bio_goofy
+            else -> R.string.bio_yensid
+        }
+
+        /*
+        * Update images and texts
+        * */
+        imageView.setImageResource(drawableResource)
+        textViewBio.setText(stringResourceBio)
     }
 }
